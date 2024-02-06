a6667f37f79abf50e190330c4eb492be7fe3b231
s-disalvo <130170596+s-disalvo@users.noreply.github.com>
2024-02-06 10:48:31 -0500

    Add files via upload

---
+++ Python Games/madlibs.py
@@ -0,0 +1,17 @@
+adj1 = input("Adjective: ")
+adj2 = input("Adjective: ")
+verb1 = input("Verb: ")
+place = input("Place: ")
+adj3 = input("Adjective: ")
+animal = input("Animal: ")
+adj4 = input("Adjective: ")
+name = input("Name: ")
+famous_person = input("Famous Person: ")
+verb2 = input("Verb: ")
+adj5 = input("Adjective: ")
+
+madlib = f"Cats are {adj1} pets to have. They can do so many things. The most {adj2} thing they can do is {verb1}! One time, I saw my cat at \
+{place} and he was acting like a {adj3} {animal}. Cats ought to have {adj4} names as well, such as {name}. That's the name of \
+{famous_person}'s cat. So {verb2} on over to your local pet shelter and pick up your new {adj5} friend!"
+
+print(madlib)
